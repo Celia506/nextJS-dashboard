7e7cafbcb3edf6fb0da0ca755c1c38586e82d501
Celia506 <celia.sansano.dwwm48@gmail.com>
2025-05-06 12:16:58 +0200

    fin chap 7

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -50,6 +50,9 @@ export async function fetchLatestInvoices() {
   }
 }
 
+
+
+
 export async function fetchCardData() {
   try {
     // You can probably combine these into a single SQL query
@@ -58,9 +61,9 @@ export async function fetchCardData() {
     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
     const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
     const invoiceStatusPromise = sql`SELECT
-         SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
-         SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
-         FROM invoices`;
+        SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
+        SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
+        FROM invoices`;
 
     const data = await Promise.all([
       invoiceCountPromise,
@@ -84,6 +87,9 @@ export async function fetchCardData() {
     throw new Error('Failed to fetch card data.');
   }
 }
+
+
+
 
 const ITEMS_PER_PAGE = 6;
 export async function fetchFilteredInvoices(
