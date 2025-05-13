f0f6f8bc240e4582bb1cf02be7806c5114d798a4
Celia506 <celia.sansano.dwwm48@gmail.com>
2025-05-13 15:16:22 +0200

    fin chap 15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,10 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
+
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 // ! indique qu’on est certain que la variable n’est pas undefined
@@ -21,6 +25,8 @@ const FormSchema = z.object({
     }),
     date: z.string(),
 });
+
+
 
 
 // ***********CREATE************************
@@ -130,4 +136,28 @@ export async function updateInvoice(
 export async function deleteInvoice(id: string) {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
+}
+
+
+
+// *********Autentification*********************************
+
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
 }
